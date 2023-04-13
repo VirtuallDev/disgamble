018e31e0f5483615f52fda8496d3374ecb0dbb2d
F1xP <f1x555111@gmail.com>
2023-04-13 20:52:27 +0300

    responsive

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import { socketRequest } from '../../apiHandler';
-
 const Main = () => {
-  const socket = async () => {
-    socketRequest('hello', 'blabla');
-  };
-
-  return (
-    <div>
-      <button
-        className="join-btn"
-        onClick={(e) => socket()}>
-        Socket
-      </button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Main;
