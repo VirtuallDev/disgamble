bd7ea8afa29e6b329cbeaeb69fcd0661b0d32e02
F1x <f1x777111@gmail.com>
2023-06-14 07:37:10 +0300

    first commit

--- server/routers/users.js
+++ server/routers/users.js
@@ -5,14 +5,6 @@ const { getUserInfoByAuthHeader } = require('../utils');
 const multer = require('multer');
 router.use(getUserInfoByAuthHeader);
 
-const adsArray = [
-  'https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg',
-  'https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=',
-  'https://gratisography.com/wp-content/uploads/2023/02/gratisography-colorful-kittenfree-stock-photo-800x525.jpg',
-  'https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=480&h=320',
-  'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-];
-
 router.get('/getuserinfo', async (req, res) => {
   try {
     const user = await User.findOne({ 'userInfo.userId': req.userID }).lean();
@@ -24,17 +16,6 @@ router.get('/getuserinfo', async (req, res) => {
     user.serverList = servers || [];
     user.ads = adsArray || [];
     return res.json({ success: user });
-  } catch (e) {
-    console.log(e);
-    return res.status(500).json({ error: 'Something went wrong!' });
-  }
-});
-
-router.get('/dmhistory', async (req, res) => {
-  try {
-    const dmhistory = await Dm.find({ recipients: { $in: [req.userID] } }).lean();
-    if (dmhistory.length === 0) return res.status(500).json({ error: 'Something went wrong!' });
-    return res.status(200).json({ success: dmhistory });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ error: 'Something went wrong!' });
@@ -63,7 +44,10 @@ const upload = multer({
 
 router.post('/upload', upload.single('file'), async (req, res) => {
   try {
-    const user = await User.findOneAndUpdate({ 'userInfo.userId': req.userID }, { 'userInfo.image': `https://doriman.yachts:5001/images/${req.imgName}` });
+    const user = await User.findOneAndUpdate(
+      { 'userInfo.userId': req.userID },
+      { 'userInfo.image': `https://doriman.yachts:5001/images/${req.imgName}` }
+    );
     if (!user) return res.status(500).json({ error: 'Something went wrong!' });
     return res.status(200).json({ success: 'Uploaded successfully' });
   } catch (e) {
